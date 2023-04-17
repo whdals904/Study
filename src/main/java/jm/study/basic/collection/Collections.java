b484dd78481a5d607deb8f674f9b7dd085190d92
김종민 <whdals904@naver.com>
2023-04-17 15:09:31 +0900

    commit

--- src/main/java/jm/study/basic/collection/Collections.java
+++ src/main/java/jm/study/basic/collection/Collections.java
@@ -1,7 +1,10 @@
 package jm.study.basic.collection;
 
+import io.swagger.models.auth.In;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Collections {
 
@@ -10,6 +13,27 @@ public class Collections {
 
         //결합도를 낮추고 의존성 분리를 위해 interface로 받는다.
         List<String> li = new ArrayList<String>();
+
+        List<Integer> stack = new Stack<Integer>();
+        stack.add(1);
+        stack.add(2);
+        stack.add(3);
+        stack.add(4);
+        stack.add(5);
+        System.out.println("stack = " + stack);
+        stack.remove(3);
+        System.out.println("stack = " + stack);
+
+        Stack<Integer> st = new Stack<Integer>();
+        st.add(1);
+        st.add(2);
+        st.add(3);
+        st.add(4);
+        st.add(5);
+        System.out.println("st = " + st);
+        st.pop();
+        System.out.println("st = " + st);
+
 
     }
 }
