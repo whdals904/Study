dfbb2d403a9d84746661c5616419dd51d7e0a186
김종민 <whdals904@naver.com>
2023-04-23 22:05:55 +0900

    commit

--- src/main/java/jm/study/program/jpa/jpaShop.java
+++ src/main/java/jm/study/program/jpa/jpaShop.java
@@ -1,15 +1,9 @@
 package jm.study.program.jpa;
 
-import jm.study.program.jpa.domain.Member;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import javax.persistence.EntityManager;
-import javax.transaction.Transactional;
 
 @SpringBootApplication
 public class jpaShop {
