a0474c6471e3d46d4cf9e309f493681935461c91
김종민 <whdals904@naver.com>
2023-04-30 21:49:56 +0900

    commit

---
+++ src/main/java/jm/study/book/doit알고리즘코딩테스트자바/Problem14.java
@@ -0,0 +1,38 @@
+package jm.study.book.doit알고리즘코딩테스트자바;
+
+
+public class Problem14 {
+    public void answer(){
+        long start = System.currentTimeMillis();
+        Integer totalLoopCnt = 0;
+        Integer count =0;
+        //todo
+
+        long end = System.currentTimeMillis();
+        System.out.println("[my]     total = " + totalLoopCnt + ", answer : " + count + ", time(ms) : " + (end-start));
+    }
+
+
+    public void my(){
+        long start = System.currentTimeMillis();
+        Integer totalLoopCnt = 0;
+        Integer count =0;
+        //todo
+
+
+        long end = System.currentTimeMillis();
+        System.out.println("[answer] total = " + totalLoopCnt + ", answer : " + count + ", time(ms) : " + (end-start));
+    }
+
+    public Problem14(String title){
+        System.out.println("====================================");
+        System.out.println(title);
+        System.out.println("====================================");
+    }
+
+    public static void main(String[] args) {
+        Problem14 p = new Problem14("절대값 힙 구현하기");
+        p.my();
+        p.answer();
+    }
+}
