50959c5adb3d1b0a9c10dd3a023047575d7aaa02
김종민 <whdals904@naver.com>
2023-05-13 00:18:42 +0900

    completableFuture MemberRepository Test add

--- src/main/java/jm/study/program/jpa/repository/MemberRepository.java
+++ src/main/java/jm/study/program/jpa/repository/MemberRepository.java
@@ -1,22 +1,46 @@
 package jm.study.program.jpa.repository;
 
 import jm.study.program.jpa.domain.Member;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Repository
+@Slf4j
+@RequiredArgsConstructor
 public class MemberRepository {
 
     @PersistenceContext
     private EntityManager entityManager;
 
+    private final ThreadPoolTaskExecutor threadPoolTaskExecutor;
+
+
+    long i = 0L;
 
     public Long save(Member m) {
         entityManager.persist(m);
         return m.getId();
+    }
+
+    public CompletableFuture<Long> saveCompatableFutureNew(Member m) {
+        entityManager.persist(m);
+        log.warn ("saveCompatableFutureNew1 =========== ");
+
+        return CompletableFuture.supplyAsync(()->{ //여기부터 별도의 스레드
+            log.warn ("saveCompatableFutureNew2 =========== " + i);
+            try{Thread.sleep(500);}catch (Exception e){e.printStackTrace();}
+            return m.getId();
+            }, threadPoolTaskExecutor
+        );
     }
     public Member findOne(Long id){
         return entityManager.find(Member.class,id);
--- src/test/java/jm/study/program/jpa/repository/MemberRepositoryTest.java
+++ src/test/java/jm/study/program/jpa/repository/MemberRepositoryTest.java
@@ -2,25 +2,44 @@ package jm.study.program.jpa.repository;
 
 import jm.study.program.jpa.domain.Address;
 import jm.study.program.jpa.domain.Member;
+import jm.study.program.jpa.service.MemberService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.test.annotation.Rollback;
 
 import javax.transaction.Transactional;
+import javax.validation.constraints.AssertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
+@Slf4j
 class MemberRepositoryTest {
     
     @Autowired
     private MemberRepository memberRepository;
 
+    @Autowired
+    private MemberService memberService;
+
+    @Autowired
+    private ThreadPoolTaskExecutor threadPoolTaskExecutor;
+
+    List<Member> list = new ArrayList<>();
+
     @Test
-    @Transactional
-    @Rollback(value = false)
     public void 멤버저장() throws Exception {
         //given
         Member m = new Member();
@@ -60,8 +79,32 @@ class MemberRepositoryTest {
         m.setAddress(a);
         //when
         Long id = memberRepository.save(m);
-
         //then
-    
+        for(int i = 0 ; i < 10 ; i++){
+            Member m1 = new Member();
+            m1.setName("김종민-" + i);
+            Address a1 = new Address("김포","김포한강8로","4300-5061");
+            m1.setAddress(a1);
+            list.add(m1);
+        }
+    }
+
+
+    @Test
+    @Rollback(value = false)
+    public void 비동기_테스트() throws Exception {
+
+        long start = System.currentTimeMillis();
+        list.forEach((m)->{
+              log.warn("inner foreach !!");
+              memberRepository.saveCompatableFutureNew(m).thenAccept(p->{
+                    log.warn("================== Receive " + p);
+               }); //여기까지 별도의 스레드
+        });
+
+        long end = System.currentTimeMillis();
+        log.warn("======================================");
+        log.warn("(end - start) = " + (end - start));
+        log.warn("======================================");
     }
 }
