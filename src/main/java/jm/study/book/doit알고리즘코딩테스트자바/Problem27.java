deebb19f5aa3dd2e6c87f327c26807cbe1461d61
김종민 <whdals904@naver.com>
2023-05-14 23:20:07 +0900

    completableFuture MemberRepository Test add

--- src/main/java/jm/study/book/doit알고리즘코딩테스트자바/Problem27.java
+++ src/main/java/jm/study/book/doit알고리즘코딩테스트자바/Problem27.java
@@ -1,17 +1,28 @@
 package jm.study.book.doit알고리즘코딩테스트자바;
 
 
+import javax.lang.model.SourceVersion;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class Problem27 {
 
+    static int N = 4;
+    static int M = 6;
 
+    static int A[][] = new int[N][M];
+    static int[] dx = {0,1,0,-1};
+    static int[] dy = {1,0,-1,0};
 
-    int ary[][] = new int[4][6];
-
+    static boolean[][] visited;
     public Problem27(){
-        ary[0] = new int[]{1,0,1,1,1,1};
+/*        ary[0] = new int[]{1,0,1,1,1,1};
         ary[1] = new int[]{1,0,1,0,1,0};
         ary[2] = new int[]{1,0,1,0,1,1};
-        ary[3] = new int[]{1,1,1,0,1,1};
+        ary[3] = new int[]{1,1,1,0,1,1};*/
+
+
     }
 
     public void answer(){
@@ -23,66 +34,64 @@ public class Problem27 {
         long end = System.currentTimeMillis();
         System.out.println("[my]     total = " + totalLoopCnt + ", answer : " + count + ", time(ms) : " + (end-start));
     }
-    public void BFS(int row, int col, int depth){
 
-        
-
-
-        if(ary[row][col+1] == 1){
-            ary[row][col+1] = depth +1;
-            BFS(row, col+1, depth +1);
-        }
-        if(col > 0 && ary[row][col-1] == 1){
-            ary[row][col-1] = depth +1;
-            BFS(row, col-1, depth +1);
-        }
-        if(ary[row+1][col] == 1){
-            ary[row+1][col] = depth +1;
-            BFS(row+1, col, depth +1);
-
-        }
-        if(row > 0 && ary[row-1][col] == 1){
-            ary[row][col-1] = depth +1;
-            BFS(row, col-1, depth +1);
-        }
-    }
 
     public void my(){
-        long start = System.currentTimeMillis();
-        Integer totalLoopCnt = 0;
-        Integer count =0;
-        //todo
-        int n = 4;
-        int m = 6;
 
 
+        BFS(0, 0);
+        System.out.println("A[N-1][M-1] = " + A[N-1][M-1]);
 
-
-
-        for(int i = 0 ; i < n ; i++) {
-            for (int j = 0; j < n; j++) {
-                BFS(i, j, 1);
-            }
-        }
-
-
-
-
-
-
-
-        long end = System.currentTimeMillis();
-        System.out.println("[answer] total = " + totalLoopCnt + ", answer : " + count + ", time(ms) : " + (end-start));
     }
+
+    public static void BFS(int i, int j){
+
+        Queue<int[]> queue= new LinkedList<>();
+        queue.offer(new int[]{i,j});
+        visited[i][j] = true;
+            while(!queue.isEmpty()){
+
+                int now[] = queue.poll();
+                for(int k = 0 ; k < 4 ; k++){
+                    int x = now[0] + dx[k];
+                    int y = now[1] + dy[k];
+
+                    if(x >= 0 && y >= 0 && x<N && y< M){
+
+                        if(A[x][y] != 0 && !visited[x][y])
+                        {
+                           visited[x][y] = true;
+                            A[x][y] = A[now[0]][now[1]] + 1;
+                            System.out.println("x,y = " +x + "," + y);
+
+                            queue.add(new int[]{x,y});
+                        }
+
+                    }
+
+
+                }
+
+
+
+            }
+    }
+
 
     public Problem27(String title){
         System.out.println("====================================");
         System.out.println(title);
         System.out.println("====================================");
+
+        A[0] = new int[]{1,1,0,1,1,0};
+        A[1] = new int[]{1,1,0,1,1,0};
+        A[2] = new int[]{1,1,1,1,1,1};
+        A[3] = new int[]{1,1,1,1,0,1};
     }
 
     public static void main(String[] args) {
         Problem27 p = new Problem27("스택과 큐");
+        visited = new boolean[N][M];
         p.my();
         p.answer();
     }
