2af31f5dacf608379c834f07cd3f2ddfd1989970
김종민 <whdals904@naver.com>
2023-05-21 16:58:43 +0900

    Doit 37번 추가

---
+++ src/main/java/jm/study/book/doit알고리즘코딩테스트자바/Problem37.java
@@ -0,0 +1,64 @@
+package jm.study.book.doit알고리즘코딩테스트자바;
+
+
+public class Problem37 {
+    public void answer(){
+        long start = System.currentTimeMillis();
+
+
+
+
+
+        long end = System.currentTimeMillis();
+        System.out.println("time(ms) : " + (end-start));
+    }
+    int M = 3;
+    int N = 16;
+
+    public void my(){
+        long start = System.currentTimeMillis();
+
+
+
+        System.out.println(Math.sqrt(3));
+        System.out.println(Math.sqrt(16));
+
+        int[] answer = new int[N+1];
+        for(int i = 2; i <= N ; i++){
+            answer[i] = i;
+        }
+
+        for(int i = 2; i <= Math.sqrt(N); i++){
+            if(answer[i] == 0){
+                continue;
+            }
+            for(int j = i+i ; j <= N ; j = j+ i){
+                System.out.println("j = " + j);
+                answer[j] = 0;
+            }
+        }
+
+        for(int i = M ; i <= N ; i++){
+            if(answer[i] != 0){
+                System.out.println("answer[i] = " + answer[i]);
+            }
+        }
+
+        long end = System.currentTimeMillis();
+        System.out.println("time(ms) : " + (end-start));
+    }
+
+
+
+    public Problem37(String title){
+        System.out.println("====================================");
+        System.out.println(title);
+        System.out.println("====================================");
+    }
+
+    public static void main(String[] args) {
+        Problem37 p = new Problem37("스택과 큐");
+        p.my();
+        p.answer();
+    }
+}
