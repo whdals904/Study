4e942e447edf12bc2b0be2168ef0223c889d6987
김종민 <whdals904@naver.com>
2023-06-02 09:52:51 +0900

    수학 소숫점 덧셈 테스트

--- src/main/java/jm/study/basic/math/Main.java
+++ src/main/java/jm/study/basic/math/Main.java
@@ -3,8 +3,26 @@ package jm.study.basic.math;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(1.1 + 0.1);
+        double value1 = 12.23;
+        double value2 = 34.45;
 
+// 기대값 : 46.68
+        System.out.println(value1 + value2); // 46.68000000000001
+        System.out.println( 12.23 + 34.45); // 46.68000000000001
+
+        System.out.println(1.1 + 0.1);
+        System.out.println(1.1 + 0.2);
+        System.out.println(1.1 + 0.4);
+        System.out.println(1.1 + 0.8);
+
+        System.out.println(1.1F);
+        System.out.println(1.2F);
+        System.out.println(1.4F);
+        System.out.println(1.8F);
+
+        System.out.println(1.1 + 0.1 == 1.2);
+        System.out.println(1.1F+ 0.1F);
+        System.out.println(1.1F+ 0.1F == 1.2F);
         float a = 1.1f;
         float b = 0.1f;
         if(a + b > 1.2){
